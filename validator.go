05aef57ef0ed3fa7b7bc0ee79203197db8a11a9e
Bobby Donev <donev.bobby@gmail.com>
2024-04-10 01:17:17 +0100

    Added a validator

---
+++ validator.go
@@ -0,0 +1,109 @@
+package firevault
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+type validator struct {
+	validations map[string]ValidationFn
+}
+
+type ValidationFn func(string, reflect.Value) error
+
+type reflectedStruct struct {
+	types  reflect.Type
+	values reflect.Value
+}
+
+func newValidator() *validator {
+	validator := &validator{make(map[string]ValidationFn)}
+
+	return validator
+}
+
+func (v *validator) validate(data interface{}) (map[string]interface{}, error) {
+	rs := reflectedStruct{reflect.TypeOf(data), reflect.ValueOf(data)}
+
+	if rs.values.Kind() != reflect.Pointer && rs.values.Kind() != reflect.Ptr {
+		return nil, errors.New("firevault: data must be a pointer to a struct")
+	}
+
+	rs.values = rs.values.Elem()
+	rs.types = rs.types.Elem()
+
+	if rs.values.Kind() != reflect.Struct {
+		return nil, errors.New("firevault: data must be a pointer to a struct")
+	}
+
+	dataMap, err := v.validateFields(rs)
+	return dataMap, err
+}
+
+func (v *validator) validateFields(rs reflectedStruct) (map[string]interface{}, error) {
+	// map which will hold all fields to pass to firestore
+	dataMap := make(map[string]interface{})
+
+	// iterate over struct fields
+	for i := 0; i < rs.values.NumField(); i++ {
+		fieldValue := rs.values.Field(i)
+		fieldType := rs.types.Field(i)
+		fieldName := fieldType.Name
+
+		// get pointer value
+		if fieldValue.Kind() == reflect.Pointer || fieldValue.Kind() == reflect.Ptr {
+			fieldValue = fieldValue.Elem()
+		}
+
+		tag := fieldType.Tag.Get("firevault")
+
+		if tag == "" || tag == "-" {
+			continue
+		}
+
+		rules := v.parseTag(tag)
+
+		// validate field based on rules
+		for _, rule := range rules {
+			if validation, ok := v.validations[rule]; ok {
+				err := validation(fieldType.Name, fieldValue)
+				if err != nil {
+					return nil, err
+				}
+			} else {
+				return nil, fmt.Errorf("firevault: unknown validation rule: %s", rule)
+			}
+		}
+
+		// If the field is a nested struct, recursively validate it and add to map
+		if fieldValue.Kind() == reflect.Struct {
+			newMap, err := v.validateFields(reflectedStruct{fieldValue.Type(), fieldValue})
+			if err != nil {
+				return nil, err
+			}
+
+			dataMap[fieldName] = newMap
+		} else {
+			dataMap[fieldName] = fieldValue.Interface()
+		}
+	}
+
+	return dataMap, nil
+}
+
+func (v *validator) parseTag(tag string) []string {
+	rules := strings.Split(tag, ",")
+
+	var validatedRules []string
+
+	for _, rule := range rules {
+		trimmedRule := strings.TrimSpace(rule)
+		if trimmedRule != "" {
+			validatedRules = append(validatedRules, trimmedRule)
+		}
+	}
+
+	return validatedRules
+}
