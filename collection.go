a63a445ef019325e87a259570d6369a1fbb7d30a
Bobby Donev <donev.bobby@gmail.com>
2024-04-28 05:11:59 +0100

    Added support for specifying fields to merge during update

--- collection.go
+++ collection.go
@@ -22,6 +22,36 @@ type CreationOptions struct {
 	Id string
 }
 
+type UpdatingOptions struct {
+	Options
+	// Specify which field paths to be overwritten. Other fields
+	// on the existing document will be untouched.
+	//
+	// It is an error if a provided field path does not refer to a value
+	// in the data passed.
+	//
+	// If left empty, all the field paths given in the data argument
+	// will be overwritten.
+	MergeFields []FieldPath
+}
+
+// FieldPath is a non-empty sequence of non-empty fields that reference a value.
+//
+// For example,
+//
+//	[]string{"a", "b"}
+//
+// is equivalent to the string form
+//
+//	"a.b"
+//
+// but
+//
+//	[]string{"*"}
+//
+// has no equivalent string form.
+type FieldPath []string
+
 // Create a new Collection instance
 func NewCollection[T interface{}](connection *Connection, name string) (*Collection[T], error) {
 	if name == "" {
@@ -104,12 +134,24 @@ func (c *Collection[T]) Find() *Query[T] {
 }
 
 // Update a Firestore document with provided id and data (after validation)
-func (c *Collection[T]) UpdateById(id string, data *T, opts ...Options) error {
+func (c *Collection[T]) UpdateById(id string, data *T, opts ...UpdatingOptions) error {
 	options := validationOpts{false, true, true}
+	mergeFields := firestore.MergeAll
 
 	if len(opts) > 0 {
 		options.skipValidation = opts[0].SkipValidation
 		options.skipRequired = opts[0].SkipRequired
+
+		if len(opts[0].MergeFields) > 0 {
+			fps := make([]firestore.FieldPath, 0)
+
+			for i := 0; i < len(opts[0].MergeFields); i++ {
+				fp := firestore.FieldPath(opts[0].MergeFields[i])
+				fps = append(fps, fp)
+			}
+
+			mergeFields = firestore.Merge(fps...)
+		}
 	}
 
 	dataMap, err := c.connection.validator.validate(data, options)
@@ -117,7 +159,7 @@ func (c *Collection[T]) UpdateById(id string, data *T, opts ...Options) error {
 		return err
 	}
 
-	_, err = c.ref.Doc(id).Set(c.connection.ctx, dataMap, firestore.MergeAll)
+	_, err = c.ref.Doc(id).Set(c.connection.ctx, dataMap, mergeFields)
 	return err
 }
 
